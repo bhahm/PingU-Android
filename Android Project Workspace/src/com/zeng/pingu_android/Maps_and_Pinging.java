c1ab7b95c890e4e2a799af213442acad9003cee6
Steven Zeng <zengyifa@umich.edu>
2014-01-20 16:45:54 -0500

    t Pleasyyyyyyythe commit message for your changes. Lines starting

--- Android Project Workspace/src/com/zeng/pingu_android/Maps_and_Pinging.java
+++ Android Project Workspace/src/com/zeng/pingu_android/Maps_and_Pinging.java
@@ -54,9 +54,27 @@ public class Maps_and_Pinging extends FragmentActivity implements
 				.getDefaultSharedPreferences(Maps_and_Pinging.this);
 		String username = prefs.getString("username", "Default NickName");
 		Useful.setUsername(username);
+<<<<<<< HEAD
 		
 		Button btnPrefs = (Button) findViewById(R.id.btnPrefs);
 		Button btnFriends = (Button) findViewById(R.id.btnFriends);
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+		/*
+		 * setUpMapIfNeeded(); try { PingActions.refreshPings(mapStored); }
+		 * catch (ParseException e1) { 
+		 * e1.printStackTrace(); }
+		 */
+=======
+=======
+>>>>>>> parent of 0efd129... GOT REFRESH TOTALLY WORKING
+		
+		Button btnPrefs = (Button) findViewById(R.id.btnPrefs);
+		Button btnFriends = (Button) findViewById(R.id.btnFriends);
+>>>>>>> parent of 0efd129... GOT REFRESH TOTALLY WORKING
+>>>>>>> 1c9aa4fb98811684205314c4ee259c177829489f
 		Button btnPing = (Button) findViewById(R.id.btnPing);
 		Button btnUnPing = (Button) findViewById(R.id.btnUnPing);
 		Button btnRefresh = (Button) findViewById(R.id.btnRefresh);
@@ -84,6 +102,7 @@ public class Maps_and_Pinging extends FragmentActivity implements
 				} else if (id == R.id.btnUnPing) {
 					setUpMapIfNeeded();
 					PingActions.unpingCurrentLocation(mapStored, latLngStored);
+<<<<<<< HEAD
 				} else if (id == R.id.btnRefresh) {
 					setUpMapIfNeeded();
 					try {
@@ -91,6 +110,17 @@ public class Maps_and_Pinging extends FragmentActivity implements
 					} catch (ParseException e) {
 						e.printStackTrace();
 					}
+=======
+<<<<<<< HEAD
+=======
+				} else if (id == R.id.btnRefresh) {
+					setUpMapIfNeeded();
+					refreshMap();
+<<<<<<< HEAD
+>>>>>>> parent of 0efd129... GOT REFRESH TOTALLY WORKING
+=======
+>>>>>>> parent of 0efd129... GOT REFRESH TOTALLY WORKING
+>>>>>>> 1c9aa4fb98811684205314c4ee259c177829489f
 				} else {
 					
 				}
@@ -100,7 +130,46 @@ public class Maps_and_Pinging extends FragmentActivity implements
 		btnFriends.setOnClickListener(listener);
 		btnPing.setOnClickListener(listener);
 		btnUnPing.setOnClickListener(listener);
+<<<<<<< HEAD
 		btnRefresh.setOnClickListener(listener);
+=======
+<<<<<<< HEAD
+=======
+		btnRefresh.setOnClickListener(listener);
+		
+	}
+	
+	
+
+	public void refreshMap() {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("CurLocPing");
+		query.findInBackground(new FindCallback<ParseObject>() {
+			public void done(List<ParseObject> results, ParseException e) {
+				if (e != null) {
+					// There was an error
+				} else {
+					for (ParseObject parseObj : results) {
+						String pingTime = (String) parseObj.get("pingTime");
+						String creator = (String) parseObj.get("creator");
+						double lat = (Double) parseObj.get("latitude");
+						double lng = (Double) parseObj.get("longitude");
+						LatLng latlng = new LatLng(lat, lng);
+						
+						mapStored.addMarker(new MarkerOptions().position(
+								latlng).title(creator +" "+ pingTime));
+						// pingObj = new PingObject(pingTime, creator,
+						//		latlng);
+						// record.add(pingObj);
+					}
+				}
+			}
+		});
+		
+	}
+
+>>>>>>> parent of 0efd129... GOT REFRESH TOTALLY WORKING
+
+>>>>>>> 1c9aa4fb98811684205314c4ee259c177829489f
 	}
 	
 	

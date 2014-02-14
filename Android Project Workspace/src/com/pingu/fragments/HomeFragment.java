85221adba47d17c49451ba833be402336bbcea33
Steven Zeng <zengyifa@umich.edu>
2014-02-14 12:20:17 -0500

    fixed up textbox color

--- Android Project Workspace/src/com/pingu/fragments/HomeFragment.java
+++ Android Project Workspace/src/com/pingu/fragments/HomeFragment.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -88,8 +89,17 @@ public class HomeFragment extends Fragment implements
 	}
 
 	private void toggleTextBoxAndPing() {
-		EditText textbox = (EditText) getActivity().findViewById(
-				R.id.messageBox);
+		Display display = getActivity().getWindowManager().getDefaultDisplay(); 
+		int width = display.getWidth();
+		EditText textbox = (EditText) getActivity().findViewById(R.id.messageBox);
+		if(width > 200)
+		{
+		    textbox.setWidth(200);
+		}
+		else
+		{	
+			textbox.setWidth(width);
+		}
 		if (textbox.getVisibility() != View.VISIBLE)
 			textbox.setVisibility(View.VISIBLE);
 		else {
